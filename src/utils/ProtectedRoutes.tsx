8f901686b79f3b8557231c305ab23f666f824acd
emmamjs-7 <emmaamjs@gmail.com>
2025-09-26 12:45:42 +0200

    used bootstrapping to prevent the frontend thinking that the user was logged out when a refreshed the page

--- src/utils/ProtectedRoutes.tsx
+++ src/utils/ProtectedRoutes.tsx
@@ -2,10 +2,16 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
 
 export default function ProtectedRoutes() {
-  const { user } = useAuth();
+  const { user, isLoading } = useAuth();
   const location = useLocation();
 
-  if (!user) return <Navigate to="/login" replace state={{ from: location }} />;
-  if (user.role !== "admin") return <Navigate to="/NotFoundPage" replace />;
+  if (isLoading) return null;
+
+  if (!user) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+  if (user.role !== "admin") {
+    return <Navigate to="/" replace />;
+  }
   return <Outlet />;
 }
